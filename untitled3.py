f7c56f7bea7a6e59ba6a9e40345719d5ee5a3e4c
Dogggy9 <dogggy9@gmail.com>
2022-10-12 18:54:26 +0300

    add bot

---
+++ untitled3.py
@@ -0,0 +1,19 @@
+import telebot
+
+TOKEN ="5721389141:AAE3hEZfKPk5NsfbG-oDnIQF4XDYLH41IM8"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#  	bot.reply_to(message, message.text)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+ 	bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
