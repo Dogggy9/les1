162cf52704054c2f432f947d3254c5f09483a4e0
Shp1ker <romaboltuzkiy@gmail.com>
2022-10-12 20:07:16 +0300

    12.10.22

--- untitled3.py
+++ untitled3.py
@@ -8,9 +8,15 @@ bot = telebot.TeleBot(TOKEN)
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
+@bot.message_handler(commands=['hi'])
+def welcome(message):
+    response = f'Привет {message.from_user.first_name}'
+    bot.send_message(message.chat.id, response)
+    
 # @bot.message_handler(func=lambda message: True)
 # def echo_all(message):
 #  	bot.reply_to(message, message.text)
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
