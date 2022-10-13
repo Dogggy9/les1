6921d6c95f410bfce4fa05bd7ede15f7dd0743ff
Dogggy9 <dogggy9@gmail.com>
2022-10-13 12:30:22 +0300

    format_13.10.22

--- untitled3.py
+++ untitled3.py
@@ -1,25 +1,29 @@
 import telebot
 
-TOKEN ="5721389141:AAE3hEZfKPk5NsfbG-oDnIQF4XDYLH41IM8"
+TOKEN = "5721389141:AAE3hEZfKPk5NsfbG-oDnIQF4XDYLH41IM8"
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing?")
+
 
 @bot.message_handler(commands=['hi'])
 def welcome(message):
     response = f'Привет {message.from_user.first_name}'
     bot.send_message(message.chat.id, response)
-    
+
+
 # @bot.message_handler(func=lambda message: True)
 # def echo_all(message):
 #  	bot.reply_to(message, message.text)
 
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda m: True)
 def echo_all(message):
- 	bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.text)
+
 
 bot.infinity_polling()
